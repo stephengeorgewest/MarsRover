b6c7cf8a873228bff7d7e1288c59efa16762e0c4
Brandon Taylor <bltpyro@gmail.com>
2009-04-30 23:27:07 +0000

    Completed quad counting for all 5 servos

--- Control/RoverEncoder/RoverEncoder.c
+++ Control/RoverEncoder/RoverEncoder.c
@@ -1,38 +1,5 @@
 // Rotary encoder code for TS-7800 using GPIO pins
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-// registers that define the function of the gpio pins in pairs - 00 makes the pair of bits GPIO
-#define PC104_A_FUNCTION 0xE8000030
-#define PC104_B_FUNCTION 0xE8000034
-#define PC104_C_FUNCTION 0xE8000038
-#define PC104_D_FUNCTION 0xE800003C
-
-// GPIO data registers
-#define PC104_A_DATA 0xE8000010
-#define PC104_B_DATA 0xE8000014
-#define PC104_C_DATA 0xE8000018
-#define PC104_D_DATA 0xE800001C
-
-// GPIO direction registers - 0 for input, 1 for output
-#define PC104_A_DIRECTION 0xE8000020
-#define PC104_B_DIRECTION 0xE8000024
-#define PC104_C_DIRECTION 0xE8000028
-#define PC104_D_DIRECTION 0xE800002C
-
-#define GPIO_INPUT 0
-#define GPIO_FUNCTION 0
-
-#define QUAD1_MASK 0x00000007
-#define QUAD2_MASK 0x00000038
-#define QUAD3_MASK 0x000001c0
-#define QUAD4_MASK 0x00000e00
-#define QUAD5_MASK 0x00007000
-
-#define DEBUG
+#include "RoverEncoder.h"
 
 main(int argc, char *argv[])
 {
@@ -46,7 +13,8 @@ main(int argc, char *argv[])
 	
 	int q1count, q2count, q3count, q4count, q5count;
 	unsigned int q1PrevQuadState, q2PrevQuadState, q3PrevQuadState, q4PrevQuadState, q5PrevQuadState;
-	unsigned int q1Data;
+	unsigned int q1Data, q2Data, q3Data, q4Data, q5Data;
+	float angle1, angle2, angle3, angle4, angle5;
 	
 	q1count = 0;
 	q2count = 0;
@@ -54,6 +22,11 @@ main(int argc, char *argv[])
 	q4count = 0;
 	q5count = 0;
 	
+	angle1 = INIT_ANGLE1;
+	angle2 = INIT_ANGLE2;
+	angle3 = INIT_ANGLE3;
+	angle4 = INIT_ANGLE4;
+	angle5 = INIT_ANGLE5;
 	//open dev/mem to access physical address space
 	if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) {
         printf("/dev/mem could not be opened.\n");
@@ -100,13 +73,15 @@ main(int argc, char *argv[])
 	//initialize quad states
 	GPIOData = *((unsigned long *) portAData);
 	q1PrevQuadState = ((GPIOData & QUAD1_MASK))&0x3;
+	q2PrevQuadState = ((GPIOData & QUAD2_MASK))&0x3;
+	q3PrevQuadState = ((GPIOData & QUAD3_MASK))&0x3;
+	q4PrevQuadState = ((GPIOData & QUAD4_MASK))&0x3;
+	q5PrevQuadState = ((GPIOData & QUAD5_MASK))&0x3;
 	
 	//continuously sample quadrature
 	while(1)
 	{
 		GPIOData = *((unsigned long *) portAData);
-		
-		//printf("GPIO Data: %x\n",GPIOData);
 		
 		//parse each encoder, 3 bits per encoder from A0 to A14, 5 encoders total
 		// First 2 bits encoding, 3rd bit reference
@@ -115,49 +90,400 @@ main(int argc, char *argv[])
 		q1Data = (GPIOData & QUAD1_MASK);
 		switch (q1Data)
 		{
-			//q1QuadState =  q1Data&0x3;
 			//in each of the first 4 cases the reference bit is zero
 			case 0:
 				if(q1PrevQuadState == 0x2)
+				{
 					q1count++;
+					angle1+=QUAD_RESOLUTION1;
+				}
 				else if(q1PrevQuadState == 0x1)
+				{
 					q1count--;			
+					angle1-=QUAD_RESOLUTION1;
+				}
+#ifdef DEBUG
 				else if(q1PrevQuadState == 0x3)
 					printf("Missed count");
+#endif
 			break;
 			case 1:
 				if(q1PrevQuadState == 0x0)
+				{
 					q1count++;
+					angle1+=QUAD_RESOLUTION1;
+				}
 				else if(q1PrevQuadState == 0x3)
+				{
 					q1count--;
+					angle1-=QUAD_RESOLUTION1;
+				}
+#ifdef DEBUG
 				else if(q1PrevQuadState == 0x2)
 					printf("Missed count");
+#endif
 			break;
 			case 2:
 				if(q1PrevQuadState == 0x3)
+				{
 					q1count++;
+					angle1+=QUAD_RESOLUTION1;
+				}
 				else if(q1PrevQuadState == 0x0)
+				{
 					q1count--;
+					angle1-=QUAD_RESOLUTION1;
+				}
+#ifdef DEBUG
 				else if(q1PrevQuadState == 0x1)
 					printf("Missed count");
+#endif
 			break;
 			case 3:
 				if(q1PrevQuadState == 0x1)
+				{
 					q1count++;
+					angle1+=QUAD_RESOLUTION1;
+				}
 				else if(q1PrevQuadState == 0x2)
+				{
 					q1count--;
+					angle1-=QUAD_RESOLUTION1;
+				}
+#ifdef DEBUG
 				else if(q1PrevQuadState == 0x0) //for debug purposes
 					printf("Missed count");
+#endif
 			break;
 			
 			//if it doesn't fall in those categories it must have a 1
 			default:
 				q1count = 0;// = the reference angle or number
+				angle1 = INIT_ANGLE1;
 		}
+#ifdef DEBUG
 		printf("Quad 1 count: %d\n",q1count);
+#endif
 		q1PrevQuadState = q1Data&0x3; //mask the last 2 bits for quad count
 		
 		//encoder 2
-		
+		q2Data = (GPIOData & QUAD2_MASK)>>3;
+		switch (q2Data)
+		{
+			//in each of the first 4 cases the reference bit is zero
+			case 0:
+				if(q2PrevQuadState == 0x2)
+				{	
+					q2count++;
+					angle2 += QUAD_RESOLUTION2;
+				}
+				else if(q2PrevQuadState == 0x1)
+				{
+					q2count--;	
+					angle2 -= QUAD_RESOLUTION2;
+				}
+#ifdef DEBUG
+				else if(q2PrevQuadState == 0x3)
+					printf("Missed count");
+#endif
+			break;
+			case 1:
+				if(q2PrevQuadState == 0x0)
+				{
+					q2count++;
+					angle2 += QUAD_RESOLUTION2;
+				}
+				else if(q2PrevQuadState == 0x3)
+				{
+					q2count--;
+					angle2 -= QUAD_RESOLUTION2;
+				}
+#ifdef DEBUG
+				else if(q2PrevQuadState == 0x2)
+					printf("Missed count");
+#endif
+			break;
+			case 2:
+				if(q2PrevQuadState == 0x3)
+				{
+					q2count++;
+					angle2 += QUAD_RESOLUTION2;
+				}
+				else if(q2PrevQuadState == 0x0)
+				{
+					q2count--;
+					angle2 -= QUAD_RESOLUTION2;
+				}
+#ifdef DEBUG
+				else if(q2PrevQuadState == 0x1)
+					printf("Missed count");
+#endif
+			break;
+			case 3:
+				if(q2PrevQuadState == 0x1)
+				{
+					q2count++;
+					angle2 += QUAD_RESOLUTION2;
+				}
+				else if(q2PrevQuadState == 0x2)
+				{
+					q2count--;
+					angle2 -= QUAD_RESOLUTION2;
+				}
+#ifdef DEBUG
+				else if(q2PrevQuadState == 0x0) //for debug purposes
+					printf("Missed count");
+#endif
+			break;
+			
+			//if it doesn't fall in those categories it must have a 1
+			default:
+				q2count = 0;// = the reference angle or number
+				angle2 = INIT_ANGLE2;
+		}
+#ifdef DEBUG
+		printf("Quad 2 count: %d\n",q2count);
+#endif
+		q2PrevQuadState = q2Data&0x3; //mask the last 2 bits for quad count
+			
+		//encoder 3
+		q3Data = (GPIOData & QUAD3_MASK)>>6;
+		switch (q3Data)
+		{
+			//in each of the first 4 cases the reference bit is zero
+			case 0:
+				if(q3PrevQuadState == 0x2)
+				{
+					q3count++;
+					angle3 += QUAD_RESOLUTION3;
+				}
+				else if(q3PrevQuadState == 0x1)
+				{
+					q3count--;		
+					angle3 -= QUAD_RESOLUTION3;	
+				}
+#ifdef DEBUG
+				else if(q3PrevQuadState == 0x3)
+					printf("Missed count");
+#endif
+			break;
+			case 1:
+				if(q3PrevQuadState == 0x0)
+				{
+					q3count++;
+					angle3 += QUAD_RESOLUTION3;
+				}
+				else if(q3PrevQuadState == 0x3)
+				{
+					q3count--;
+					angle3 -= QUAD_RESOLUTION3;
+				}
+#ifdef DEBUG
+				else if(q3PrevQuadState == 0x2)
+					printf("Missed count");
+#endif
+			break;
+			case 2:
+				if(q3PrevQuadState == 0x3)
+				{
+					q3count++;
+					angle3 += QUAD_RESOLUTION3;
+				}
+				else if(q3PrevQuadState == 0x0)
+				{
+					q3count--;
+					angle3 -= QUAD_RESOLUTION3;
+				}
+#ifdef DEBUG
+				else if(q3PrevQuadState == 0x1)
+					printf("Missed count");
+#endif
+			break;
+			case 3:
+				if(q3PrevQuadState == 0x1)
+				{
+					q3count++;
+					angle3 += QUAD_RESOLUTION3;
+				}
+				else if(q3PrevQuadState == 0x2)
+				{
+					q3count--;
+					angle3 -= QUAD_RESOLUTION3;
+				}
+#ifdef DEBUG
+				else if(q3PrevQuadState == 0x0) //for debug purposes
+					printf("Missed count");
+#endif
+			break;
+			
+			//if it doesn't fall in those categories it must have a 1
+			default:
+				q3count = 0;// = the reference angle or number
+				angle3 = INIT_ANGLE3;
+		}
+#ifdef DEBUG
+		printf("Quad 3 count: %d\n",q3count);
+#endif
+		q3PrevQuadState = q3Data&0x3; //mask the last 2 bits for quad count
+			
+		//encoder 4
+		q4Data = (GPIOData & QUAD4_MASK)>>9;
+		switch (q4Data)
+		{
+			//in each of the first 4 cases the reference bit is zero
+			case 0:
+				if(q4PrevQuadState == 0x2)
+				{
+					q4count++;
+					angle4 += QUAD_RESOLUTION4;
+				}
+				else if(q4PrevQuadState == 0x1)
+				{
+					q4count--;	
+					angle4 -= QUAD_RESOLUTION4;		
+				}
+#ifdef DEBUG
+				else if(q4PrevQuadState == 0x3)
+					printf("Missed count");
+#endif
+			break;
+			case 1:
+				if(q4PrevQuadState == 0x0)
+				{
+					q4count++;
+					angle4 += QUAD_RESOLUTION4;
+				}
+				else if(q4PrevQuadState == 0x3)
+				{
+					q4count--;
+					angle4 -= QUAD_RESOLUTION4;
+				}
+#ifdef DEBUG
+				else if(q4PrevQuadState == 0x2)
+					printf("Missed count");
+#endif
+			break;
+			case 2:
+				if(q4PrevQuadState == 0x3)
+				{
+					q4count++;
+					angle4 += QUAD_RESOLUTION4;
+				}
+				else if(q4PrevQuadState == 0x0)
+				{
+					q4count--;
+					angle4 -= QUAD_RESOLUTION4;
+				}
+#ifdef DEBUG
+				else if(q4PrevQuadState == 0x1)
+					printf("Missed count");
+#endif
+			break;
+			case 3:
+				if(q4PrevQuadState == 0x1)
+				{
+					q4count++;
+					angle4 += QUAD_RESOLUTION4;
+				}
+				else if(q4PrevQuadState == 0x2)
+				{
+					q4count--;
+					angle4 -= QUAD_RESOLUTION4;
+				}
+#ifdef DEBUG
+				else if(q4PrevQuadState == 0x0) //for debug purposes
+					printf("Missed count");
+#endif
+			break;
+			
+			//if it doesn't fall in those categories it must have a 1
+			default:
+				q4count = 0;// = the reference angle or number
+				angle4 = INIT_ANGLE4;
+		}
+#ifdef DEBUG
+		printf("Quad 4 count: %d\n",q4count);
+#endif
+		q4PrevQuadState = q4Data&0x3; //mask the last 2 bits for quad count
+			
+		//encoder 5
+		q5Data = (GPIOData & QUAD5_MASK)>>12;
+		switch (q5Data)
+		{
+			//in each of the first 4 cases the reference bit is zero
+			case 0:
+				if(q5PrevQuadState == 0x2)
+				{
+					q5count++;
+					angle5 += QUAD_RESOLUTION5;	
+				}
+				else if(q5PrevQuadState == 0x1)
+				{
+					q5count--;		
+					angle5 -= QUAD_RESOLUTION5;	
+				}
+#ifdef DEBUG
+				else if(q5PrevQuadState == 0x3)
+					printf("Missed count");
+#endif
+			break;
+			case 1:
+				if(q5PrevQuadState == 0x0)
+				{
+					q5count++;
+					angle5 += QUAD_RESOLUTION5;	
+				}
+				else if(q5PrevQuadState == 0x3)
+				{
+					q5count--;	
+					angle5 -= QUAD_RESOLUTION5;	
+				}
+#ifdef DEBUG
+				else if(q5PrevQuadState == 0x2)
+					printf("Missed count");
+#endif
+			break;
+			case 2:
+				if(q5PrevQuadState == 0x3)
+				{
+					q5count++;
+					angle5 += QUAD_RESOLUTION5;	
+				}
+				else if(q5PrevQuadState == 0x0)
+				{
+					q5count--;	
+					angle5 -= QUAD_RESOLUTION5;	
+				}
+#ifdef DEBUG
+				else if(q5PrevQuadState == 0x1)
+					printf("Missed count");
+#endif
+			break;
+			case 3:
+				if(q5PrevQuadState == 0x1)
+				{
+					q5count++;	
+					angle5 += QUAD_RESOLUTION5;	
+				}
+				else if(q5PrevQuadState == 0x2)
+				{
+					q5count--;	
+					angle5 -= QUAD_RESOLUTION5;	
+				}
+#ifdef DEBUG
+				else if(q5PrevQuadState == 0x0) //for debug purposes
+					printf("Missed count");
+#endif
+			break;
+			
+			//if it doesn't fall in those categories it must have a 1
+			default:
+				q5count = 0;// = the reference angle or number
+				angle5 = INIT_ANGLE5;
+		}
+#ifdef DEBUG
+		printf("Quad 5 count: %d\n",q5count);
+#endif
+		q5PrevQuadState = q5Data&0x3; //mask the last 2 bits for quad count
 	} //end while loop
 }
